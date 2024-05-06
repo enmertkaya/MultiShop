2ffe7fde5115f1463af307b673fb1336cb532863
Mert Kaya <merteneskaya outlook.com>
2024-05-06 09:39:25 +0300

    MongoDb appsettingJson düzenlemeleri

--- Services/Catalog/MultiShop.Catalog/Entities/Category.cs
+++ Services/Catalog/MultiShop.Catalog/Entities/Category.cs
@@ -1,6 +1,15 @@
-﻿namespace MultiShop.Catalog.Entities
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
 {
     public class Category
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+
     }
 }
