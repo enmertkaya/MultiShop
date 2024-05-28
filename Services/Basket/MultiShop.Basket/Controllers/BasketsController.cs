5956304a0ad3bf0df3fbee8fea689c2cf0f8b2b2
Enes Mert Kaya <merteneskaya@outlook.com>
2024-05-28 12:26:42 +0300

    basket mikroservis olusturulmasi

---
+++ Services/Basket/MultiShop.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Security.Claims;
+
+namespace MultiShop.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketsController : ControllerBase
+    {
+        
+
+        
+    }
+}
