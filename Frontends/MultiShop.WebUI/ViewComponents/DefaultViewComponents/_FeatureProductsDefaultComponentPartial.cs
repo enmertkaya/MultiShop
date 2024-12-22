adf0372064db3af839b96d454cb5d1ff7418a104
Enes Mert Kaya <merteneskaya@outlook.com>
2024-12-22 12:57:16 +0300

    slider viewcomponent

---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_FeatureProductsDefaultComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using MultiShop.DtoLayer.CatalogDtos.ProductDtos;
+//using MultiShop.WebUI.Services.CatalogServices.ProductServices;
+//using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _FeatureProductsDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        //private readonly IProductService _productService;
+        //public _FeatureProductsDefaultComponentPartial(IProductService productService)
+        //{
+        //    _productService = productService;
+        //}
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    var values = await _productService.GetAllProductAsync();
+        //    return View(values);
+        //}
+    }
+}
---
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpeacialOfferComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+//using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+//using MultiShop.WebUI.Services.CatalogServices.SpecialOfferServices;
+//using Newtonsoft.Json;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
+{
+    public class _SpeacialOfferComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+        //private readonly ISpecialOfferService _specialOfferService;
+        //public _SpeacialOfferComponentPartial(ISpecialOfferService specialOfferService)
+        //{
+        //    _specialOfferService = specialOfferService;
+        //}
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    var values = await _specialOfferService.GetAllSpecialOfferAsync();
+        //    return View(values);
+        //}
+    }
+}
