8a8d2cf69e30d4a6eed2d1e1bb1e5a0ff3cd6673
Enes Mert Kaya <merteneskaya@outlook.com>
2024-12-30 19:46:47 +0300

    güncellenecek verilerin getirilmesi

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -78,5 +78,26 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             }
             return View();
         }
+
+        
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> UpdateCategory (string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfasi";
+            ViewBag.v0 = "Kategori İşlemleri";
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/Categories/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View ();
+        }
+
     }
 }
