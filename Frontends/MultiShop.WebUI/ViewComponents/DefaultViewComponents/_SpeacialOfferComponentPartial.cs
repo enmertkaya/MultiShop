700f17612d2e29b0da691d68eb873a4bcc285570
Enes Mert Kaya <merteneskaya@outlook.com>
2025-01-08 18:12:33 +0300

    special offer default sayfasi

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpeacialOfferComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/_SpeacialOfferComponentPartial.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
+using Newtonsoft.Json;
 //using MultiShop.DtoLayer.CatalogDtos.SpecialOfferDtos;
 //using MultiShop.WebUI.Services.CatalogServices.SpecialOfferServices;
 //using Newtonsoft.Json;
@@ -7,19 +9,28 @@ namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class _SpeacialOfferComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _SpeacialOfferComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/SpecialOffers");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultSpecialOfferDto>>(jsonData);
+                return View(values);
+            }
+
+
             return View();
         }
-        //private readonly ISpecialOfferService _specialOfferService;
-        //public _SpeacialOfferComponentPartial(ISpecialOfferService specialOfferService)
-        //{
-        //    _specialOfferService = specialOfferService;
-        //}
-        //public async Task<IViewComponentResult> InvokeAsync()
-        //{
-        //    var values = await _specialOfferService.GetAllSpecialOfferAsync();
-        //    return View(values);
-        //}
     }
 }
